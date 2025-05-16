519aa8664cbbc94ad8eb929c1e3344a09cd069bb
Seth Samuel <Seths@missionreadyhq.com>
2025-05-16 15:08:27 +1200

    setup middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,36 @@
+// ------------------ SETUP AND INSTALL ----------------- //
+
+
+require("dotenv").config();
 const { GoogleAuth } = require('google-auth-library');
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
+// --------------------- MIDDLEWARES -------------------- //
+
+const morgan = require("morgan");
+
+const corsConfigs = {
+  origin: (incomingOrigin, allowedAccess) => {
+    //we add regex of sites we want to allow here
+    const allowedOrigins = [/^http:\/\/localhost:\d+$/];
+
+    //some requests dont have an origin so we allow those through.
+    //we scan the lists of allowed origins and if it works we allow them through
+    if (!incomingOrigin || allowedOrigins.some((testOrigin) => testOrigin.test(incomingOrigin))) {
+      allowedAccess(null, true);
+    } else {
+      allowedAccess(null, false);
+    }
+  },
+};
+
+app.use(cors(corsConfigs));
+app.use(morgan("dev"));
+
+// ---------------------- FUNCTIONS --------------------- //
+
 
 async function getAccessToken() {
   const auth = new GoogleAuth({
@@ -16,6 +45,8 @@ async function getAccessToken() {
 // --------------------- ENTRYPOINT --------------------- //
 
 async function main() {
+  
+
   const tokenGet = await getAccessToken();
   console.log(tokenGet);
 }
