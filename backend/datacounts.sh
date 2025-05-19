5dd1a708791c49e416fc99ad065c7c3a0b93437e
Seth Samuel <Seths@missionreadyhq.com>
2025-05-19 15:34:05 +1200

    checkpoint

--- backend/datacounts.sh
+++ backend/datacounts.sh
@@ -1,11 +1,107 @@
-#!/bin/bash 
-set -ex
+# #!/bin/bash 
+# set -ex
 
-grep -c sedan renamed.csv
-grep -c hatchback renamed.csv
-grep -c SUV renamed.csv
-grep -c Convertable renamed.csv
-grep -c Station Wagon renamed.csv
-grep -c Pickup renamed.csv
-grep -c Van renamed.csv
-grep -c 3dr renamed.csv
+# grep -c sedan renamed.csv
+# grep -c hatchback renamed.csv
+# grep -c SUV renamed.csv
+# grep -c Convertible renamed.csv
+# grep -c "Station\ Wagon" renamed.csv
+# grep -c Pickup renamed.csv
+# grep -c Van renamed.csv
+# grep -c 3dr renamed.csv
+
+# echo "---------------------------------"
+
+# grep -c Acura renamed.csv
+# grep -c "Alfa\ Romeo" renamed.csv
+# grep -c "Aston\ Martin" renamed.csv
+# grep -c Audi renamed.csv
+# grep -c Bentley renamed.csv
+# grep -c BMW renamed.csv
+# grep -c Buick renamed.csv
+# grep -c Cadillac renamed.csv
+# grep -c Chevrolet renamed.csv
+# grep -c Chrysler renamed.csv
+# grep -c Dodge renamed.csv
+# grep -c Ferrari renamed.csv
+# grep -c FIAT renamed.csv
+# grep -c Ford renamed.csv
+# grep -c Genesis renamed.csv
+# grep -c GMC renamed.csv
+# grep -c Honda renamed.csv
+# grep -c Hyundai renamed.csv
+# grep -c INFINITI renamed.csv
+# grep -c Jaguar renamed.csv
+# grep -c Jeep renamed.csv
+# grep -c Kia renamed.csv
+# grep -c Lamborghini renamed.csv
+# grep -c "Land\ Rover" renamed.csv
+# grep -c Lexus renamed.csv
+# grep -c Lincoln renamed.csv
+# grep -c Maserati renamed.csv
+# grep -c Mazda renamed.csv
+# grep -c McLaren renamed.csv
+# grep -c Mercedes-Benz renamed.csv
+# grep -c MINI renamed.csv
+# grep -c Mitsubishi renamed.csv
+# grep -c Nissan renamed.csv
+# grep -c Porsche renamed.csv
+# grep -c Ram renamed.csv
+# grep -c Rolls-Royce renamed.csv
+# grep -c smart renamed.csv
+# grep -c Subaru renamed.csv
+# grep -c Tesla renamed.csv
+# grep -c Toyota renamed.csv
+# grep -c Volkswagen renamed.csv
+# grep -c Volvo renamed.csv
+ echo "sedan:" $(grep -c sedan renamed.csv)
+ echo "SUV:" $(grep -c SUV renamed.csv)
+ echo "hatchback:" $(grep -c hatchback renamed.csv)
+ echo "Convertible:" $(grep -c Convertible renamed.csv)
+ echo "Station Wagon:" $(grep -c "Station\ Wagon" renamed.csv)
+ echo "Pickup:" $(grep -c Pickup renamed.csv)
+ echo "Van:" $(grep -c Van renamed.csv)
+ echo "3dr:" $(grep -c 3dr renamed.csv)
+echo "---------------------------------------------"
+ echo "Acura:" $(grep -c Acura renamed.csv)
+ echo "Alfa Romeo:" $(grep -c "Alfa\ Romeo" renamed.csv)
+ echo "Aston Martin:" $(grep -c "Aston\ Martin" renamed.csv)
+ echo "Audi:" $(grep -c Audi renamed.csv)
+ echo "Bentley:" $(grep -c Bentley renamed.csv)
+ echo "BMW:" $(grep -c BMW renamed.csv)
+ echo "Buick:" $(grep -c Buick renamed.csv)
+ echo "Cadillac:" $(grep -c Cadillac renamed.csv)
+ echo "Chevrolet:" $(grep -c Chevrolet renamed.csv)
+ echo "Chrysler:" $(grep -c Chrysler renamed.csv)
+ echo "Dodge:" $(grep -c Dodge renamed.csv)
+ echo "Ferrari:" $(grep -c Ferrari renamed.csv)
+ echo "FIAT:" $(grep -c FIAT renamed.csv)
+ echo "Ford:" $(grep -c Ford renamed.csv)
+ echo "Genesis:" $(grep -c Genesis renamed.csv)
+ echo "GMC:" $(grep -c GMC renamed.csv)
+ echo "Honda:" $(grep -c Honda renamed.csv)
+ echo "Hyundai:" $(grep -c Hyundai renamed.csv)
+ echo "INFINITI:" $(grep -c INFINITI renamed.csv)
+ echo "Jaguar:" $(grep -c Jaguar renamed.csv)
+ echo "Jeep:" $(grep -c Jeep renamed.csv)
+ echo "Kia:" $(grep -c Kia renamed.csv)
+ echo "Lamborghini:" $(grep -c Lamborghini renamed.csv)
+ echo "Land Rover:" $(grep -c "Land\ Rover" renamed.csv)
+ echo "Lexus:" $(grep -c Lexus renamed.csv)
+ echo "Lincoln:" $(grep -c Lincoln renamed.csv)
+ echo "Maserati:" $(grep -c Maserati renamed.csv)
+ echo "Mazda:" $(grep -c Mazda renamed.csv)
+ echo "McLaren:" $(grep -c McLaren renamed.csv)
+ echo "Mercedes-Benz:" $(grep -c Mercedes-Benz renamed.csv)
+ echo "MINI:" $(grep -c MINI renamed.csv)
+ echo "Mitsubishi:" $(grep -c Mitsubishi renamed.csv)
+ echo "Nissan:" $(grep -c Nissan renamed.csv)
+ echo "Porsche:" $(grep -c Porsche renamed.csv)
+ echo "Ram:" $(grep -c Ram renamed.csv)
+ echo "Rolls-Royce:" $(grep -c Rolls-Royce renamed.csv)
+ echo "smart:" $(grep -c smart renamed.csv)
+ echo "Subaru:" $(grep -c Subaru renamed.csv)
+ echo "Tesla:" $(grep -c Tesla renamed.csv)
+ echo "Toyota:" $(grep -c Toyota renamed.csv)
+ echo "Volkswagen:" $(grep -c Volkswagen renamed.csv)
+ echo "Volvo:" $(grep -c Volvo renamed.csv)
--- backend/processData.js
+++ backend/processData.js
@@ -1,29 +1,36 @@
-const fs = require('fs');
-const { type } = require('os');
+const fs = require("fs");
+const { type } = require("os");
 
-const directoryPath = "C:\\Users\\fluffy\\Desktop\\dataset"
+const directoryPath = "C:\\Users\\fluffy\\Desktop\\dataset";
 const fileList = fs.readdirSync(directoryPath);
-let csvFile= ""
+let csvFile = "";
 
 const carType = new Map();
 const carBrand = new Map();
 
 // console.log("files and folders in dir: ",fileList)
 
-for (const name of fileList){
-    const fileName = name.split("_")
-    // console.log(fileName[fileName.length-2])
-    carType.set(fileName[fileName.length-2],name)
-    carBrand.set(fileName[0],name)
+for (const name of fileList) {
+  const fileName = name.split("_");
+  // console.log(fileName[fileName.length-2])
+  carType.set(fileName[fileName.length - 2], name);
+  carBrand.set(fileName[0], name);
 
-    csvFile += `gs://cars_raw/${name},${fileName[0]},${fileName[fileName.length-2]}\n`
+  csvFile += `gs://cars_raw/${name},${fileName[0]},${fileName[fileName.length - 2]}\n`;
 }
 
 // console.log(carMap)
 
-carMap.forEach((value,key)=>{
-    console.log(`${key}: ${value.length}`)
-})
+carType.forEach((value, key) => {
+  console.log(` echo "${key}:" $(grep -c ${key} renamed.csv)`)
+});
+
+console.log("---------------------------------------------")
+
+carBrand.forEach((value, key) => {
+//   console.log(`${key}: ${value.length}`);
+console.log(` echo "${key}:" $(grep -c ${key} renamed.csv)`)
+});
 
 // console.log(first)
 
